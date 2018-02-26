cc301cd95cda60263c782483fa696e8877d6c426
Andre Arko <andre@arko.net>
2018-02-25 23:18:40 -0800

    add auth tokens model and user module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+    :trackable, :validatable, :token_authenticatable
+
+  has_many :authentication_tokens
 end
