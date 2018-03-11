fcf37ca6e8422a09fabafa3f32b9f48f9b6e0a1e
Andre Arko <andre@arko.net>
2018-03-10 20:46:21 -0800

    add json format to friends controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,11 +2,13 @@ class FriendsController < ApplicationController
   before_action :set_friend, only: [:show, :edit, :update, :destroy]
 
   # GET /friends
+  # GET /friends.json
   def index
     @friends = current_user.friends
   end
 
   # GET /friends/1
+  # GET /friends/1.json
   def show
   end
 
@@ -20,6 +22,7 @@ class FriendsController < ApplicationController
   end
 
   # POST /friends
+  # POST /friends.json
   def create
     respond_to do |format|
       format.json do
@@ -45,18 +48,27 @@ class FriendsController < ApplicationController
   end
 
   # PATCH/PUT /friends/1
+  # PATCH/PUT /friends/1.json
   def update
-    if @friend.update(friend_params)
-      redirect_to @friend, notice: 'Friend was successfully updated.'
-    else
-      render :edit
+    respond_to do |format|
+      if @friend.update(friend_params)
+        format.html { redirect_to @friend, notice: 'Friend was successfully updated.' }
+        format.json { render :show, status: :ok, location: @friend }
+      else
+        format.html { render :edit }
+        format.json { render json: @friend.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /friends/1
+  # DELETE /friends/1.json
   def destroy
     @friend.destroy
-    redirect_to friends_url, notice: 'Friend was successfully destroyed.'
+    respond_to do |format|
+      format.html { redirect_to friends_url, notice: 'Friend was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
