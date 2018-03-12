2635bcee036ebe481adfc0c35557aa89ef95894c
Andre Arko <andre@arko.net>
2018-03-11 19:37:04 -0700

    break out an about page and send users to friends

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to friends_path
+    else
+      redirect_to about_path
+    end
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     resources :catchups
   end
 
+  get '/about' => 'home#about'
   root to: "home#index"
 end
