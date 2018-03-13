66fc6a43c548c520d6864114646720152746b1d4
Andre Arko <andre@arko.net>
2018-03-13 01:12:00 -0700

    Something that resembles working webpacker fonts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,8 @@
-import "./application.css"
+import "assets/fonts/nunitosans-regular.ttf";
+import "assets/fonts/fredokaone.ttf";
+
+import "assets/stylesheets/application";
+
 import Rails from "rails-ujs";
 import Turbolinks from "turbolinks";
 
