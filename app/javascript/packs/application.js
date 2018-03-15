91c58e053e785013016ff0da9fbf6f1f59421d34
Andre Arko <andre@arko.net>
2018-03-15 01:39:48 -0700

    omg per-env css that doesn't blow up in prod

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,11 @@
 import "fonts/nunitosans-regular.ttf";
 import "fonts/fredokaone.ttf";
 import "images/logo_sketch.png";
-import "stylesheets/application.css";
 
-if (["development"].includes(process.env.RAILS_ENV)) {
-  require(`stylesheets/${process.env.RAILS_ENV}.css`);
-}
+// Stylesheets
+import "stylesheets/application.css";
+/* Environment-specific CSS, if it exists */
+import(/* webpackMode: "eager" */ `stylesheets/${process.env.RAILS_ENV}.css`);
 
 import Rails from "rails-ujs";
 import Turbolinks from "turbolinks";
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,24 @@
 const { environment } = require('@rails/webpacker')
 
+function environmentCssAlias() {
+  // If there is no environment-specific CSS for the current environment, alias the
+  // non-existent file to the main application CSS file so webpack won't blow up.
+  const fs = require('fs');
+  const path = require('path');
+  let envCss = path.resolve(__dirname, `../../app/assets/stylesheets/${process.env.RAILS_ENV}.css`);
+  if (!fs.existsSync(envCss)) {
+    envCss = path.resolve(__dirname, "../../app/assets/stylesheets/application.css");
+  }
+
+  return {
+    resolve: {
+      alias: {
+        [`stylesheets/${process.env.RAILS_ENV}.css$`]: envCss,
+      }
+    }
+  }
+};
+
+environment.config.merge(environmentCssAlias())
+
 module.exports = environment
