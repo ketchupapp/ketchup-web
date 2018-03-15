406e5424b16154091e4919562c8e579f08826198
Andre Arko <andre@arko.net>
2018-03-15 01:40:05 -0700

    pull out app-specific JS into ketchup.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,14 @@
-import "fonts/nunitosans-regular.ttf";
-import "fonts/fredokaone.ttf";
-import "images/logo_sketch.png";
+// Application JS
+import "ketchup.js";
 
 // Stylesheets
 import "stylesheets/application.css";
 /* Environment-specific CSS, if it exists */
 import(/* webpackMode: "eager" */ `stylesheets/${process.env.RAILS_ENV}.css`);
 
-import Rails from "rails-ujs";
-import Turbolinks from "turbolinks";
+// Fonts
+import "fonts/nunitosans-regular.ttf";
+import "fonts/fredokaone.ttf";
 
-Rails.start();
-Turbolinks.start();
+// Images
+import "images/logo_sketch.png";
