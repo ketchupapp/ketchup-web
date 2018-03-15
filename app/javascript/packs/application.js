774c22ab12193d7031679187f38f2fff1806d4c5
Andre Arko <andre@arko.net>
2018-03-14 18:23:12 -0700

    sketchy logo, sketchy font for local dev

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,11 @@
 import "fonts/nunitosans-regular.ttf";
 import "fonts/fredokaone.ttf";
-
+import "images/logo_sketch.png";
 import "stylesheets/application.css";
+
+if (["development"].includes(process.env.RAILS_ENV)) {
+  require(`stylesheets/${process.env.RAILS_ENV}.css`);
+}
 
 import Rails from "rails-ujs";
 import Turbolinks from "turbolinks";
