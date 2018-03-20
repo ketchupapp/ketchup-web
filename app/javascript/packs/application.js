214f77146376bd0be5190ea5b4fa37ed45afd4e9
Andre Arko <andre@arko.net>
2018-03-19 22:38:32 -0700

    Pull in tachyons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ import "ketchup.js";
 
 // Stylesheets
 import "stylesheets/ketchup.css";
+import "tachyons/css/tachyons.css";
 /* Environment-specific CSS, if it exists */
 import(/* webpackMode: "eager" */ `stylesheets/${process.env.RAILS_ENV}.css`);
 
