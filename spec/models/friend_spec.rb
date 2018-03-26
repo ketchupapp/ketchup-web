81a947dc84e352b2d718078f82a17e8db8a2501f
Andre Arko <andre@arko.net>
2018-03-26 01:23:34 -0700

    get friend specs passing again

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Friend, type: :model do
   let(:user) { User.new(email: "sue@example.com", password: "abc123") }
-  subject(:friend) { Friend.new(user: user) }
+  subject(:friend) { Friend.new(user: user, name: "Alice") }
 
   describe "validations" do
     it "allow saving" do
@@ -14,10 +14,10 @@ RSpec.describe Friend, type: :model do
     it "returns the most recent catchup record" do
       friend.save!
 
-      last = friend.catchups.create!(user: friend.user, happened_at: Time.now - 100)
+      last = friend.catchups.create!(happened_at: Time.now - 100)
       expect(friend.reload.last_catchup).to eq(last)
 
-      new_last = friend.catchups.create!(user: friend.user, happened_at: Time.now)
+      new_last = friend.catchups.create!(happened_at: Time.now)
       expect(friend.reload.last_catchup).to eq(new_last)
     end
   end
