8b14a34f9f75009ab0ba1a8c2e37b9de7330ced1
Andre Arko <andre@arko.net>
2018-03-26 01:26:44 -0700

    stop hitting the pwned API during tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-    :trackable, :validatable, :token_authenticatable, :pwned_password
+    :trackable, :validatable, :token_authenticatable
+  devise :pwned_password unless Rails.env.test?
 
   has_many :authentication_tokens, dependent: :destroy
   has_many :friends, dependent: :destroy
