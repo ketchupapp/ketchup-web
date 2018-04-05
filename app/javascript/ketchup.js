2f609870332dd62722f0fa81132f2b9c2e93efb7
Andre Arko <andre@arko.net>
2018-04-05 05:31:50 +0000

    use activestorage-cloudinary js for direct uploads
    this JS works together with the forked activestorage service, and now
    we have direct uploads. hooray!

--- app/javascript/ketchup.js
+++ app/javascript/ketchup.js
@@ -1,6 +1,6 @@
 import Rails from "rails-ujs";
 import Turbolinks from "turbolinks";
-import * as ActiveStorage from "activestorage";
+import * as ActiveStorage from "activestorage-cloudinary";
 
 Rails.start();
 Turbolinks.start();
